04d482fb99ddad18c5e87c477d9618881fb92fa2
Marc Daya <marc@sinistral.net>
2014-06-02 17:39:57 +0200

    Macro: `returning`.
    Returning is similar to `doto`, but closer perhaps to Ruby's `tap`.  It
    allows for arbitrary forms to be evaluated before returning the captured
    value.

--- src/mantle/core.clj
+++ src/mantle/core.clj
@@ -1,6 +1,14 @@
-(ns mantle.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns mantle.core
+  "In which is defined utility macros and functions that wrap the core Clojure
+  language to provide additional functionality or convenience.")
+
+(defmacro returning
+  "Takes a single binding, executes `forms` in the context of that
+  binding, and returns `val`.  Although similar in spirit to `doto`,
+  it allows for arbitrary forms to be evaluated before returning
+  `val`, and is thus closer to Ruby's `tap`."
+  [[var val] & body]
+  `(let [~var ~val]
+     ~@body
+     ~var))
--- test/mantle/core_test.clj
+++ test/mantle/core_test.clj
@@ -1,7 +1,18 @@
+
 (ns mantle.core-test
   (:require [clojure.test :refer :all]
             [mantle.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test:returning
+  (testing "standalone form"
+    (is (= 42 (returning [x 42]))))
+  (testing "binding is available to body"
+    (let [y (atom 0)]
+      (is (= 42 (returning [x 42]
+                  (reset! y (inc x)))))
+      (is (= 43 @y))))
+  (testing "returns the var, not the value"
+    (let [y (atom nil)]
+      (is (= (returning [x (Object.)]
+               (reset! y x))
+             @y)))))
