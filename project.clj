6f252e741551f21e7665c2fe6ab0e8987c044176
Marc Daya <marc@sinistral.net>
2015-12-08 14:16:03 -0700

    Update project documentation for v0.1.0.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 
-(defproject mantle "0.0.1-SNAPSHOT"
-  :description "Extends the core Clojure library with additional utility functions."
+(defproject sinistral/mantle "0.1.0"
+  :description "Extends the core Clojure library with additional utility or
+  convenience functions."
+  :url "https://github.com/sinistral/mantle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.5.0"]])
