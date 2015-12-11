33f01c67ad61666e12439767c4e9647da0feb5da
Marc Daya <marc@sinistral.net>
2015-12-11 15:48:56 -0700

    Fix `fmtstr`.
    Fix and tests for `fmtstr`, which was accidentally leaked before being
    tested.

---
+++ test/mantle/io_test.clj
@@ -0,0 +1,15 @@
+
+(ns mantle.io-test
+  (:require [clojure.test :refer :all]
+            [mantle.core :refer [returning]]
+            [mantle.io :refer :all])
+  (:import [java.io StringWriter]))
+
+(deftest test:format
+  (is (= ":foo" (format nil "~a" :foo)))
+  (is (= ":foo :bar" (format nil "~a ~a" :foo :bar)))
+  (is (= ":foo" (str (returning [w (StringWriter.)] (format w "~a" :foo))))))
+
+(deftest test:fmtstr
+  (is (= ":foo" (fmtstr "~a" :foo)))
+  (is (= ":foo :bar" (fmtstr "~a ~a" :foo :bar))))
