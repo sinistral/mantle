5fdddc56f18fad15c7cbfaf41bd5700a2b76a0d3
Marc Daya <marc@sinistral.net>
2016-01-11 15:02:06 -0700

    Function: select-values.
    Select from a map the values identified by a collection of keys.  The
    values are returned in the order in which the keys are specified.
    
    Full credit to Jay Fields: http://blog.jayfields.com/2011/01/clojure-select-keys-select-values-and.html

--- src/mantle/collection.clj
+++ src/mantle/collection.clj
@@ -3,6 +3,14 @@
   "In which is defined utility macros and functions that provide additional
   functionality or convenience functions for Clojure collections.")
 
+(defn select-values
+  "Returns a sequence of only the values in `map` for which there keys in
+  `keyseq`. Values are returned in the order in which the keys are
+  specified. `nil`s are included for keys not present in the map; use `(remove
+  nil? (select-values ...))` to filter them out."
+  [map keyseq]
+  (reduce #(conj %1 (map %2)) [] keyseq))
+
 (defn single
   "Returns the only element of a collection, and throws an exception if there
   is not exactly one element in the sequence."
--- test/mantle/collection_test.clj
+++ test/mantle/collection_test.clj
@@ -3,6 +3,14 @@
   (:require [clojure.test :refer :all]
             [mantle.collection :refer :all]))
 
+(deftest test:select-values
+  (let [m {:c 3 :a 1 :b 2}]
+    (is (= [3] (select-values m [:c])))
+    (is (= [1 2] (select-values m [:a :b])))
+    (is (= [2 1] (select-values m [:b :a])))
+    (is (= [3 nil] (select-values m [:c :d])))
+    (is (= [nil 3] (select-values m [:d :c])))))
+
 (deftest test:single
   (letfn [(test-single-modes [<1 =1 >1]
             (is (thrown-with-msg? AssertionError #"found an empty collection"
