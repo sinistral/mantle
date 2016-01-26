08d8fc01ad313cb9f96aa0a1ba28b4e7425cb5b8
Marc Daya <marc@sinistral.net>
2016-01-26 08:22:15 -0700

    Update project version: 0.3.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 
-(defproject sinistral/mantle "0.2.0-SNAPSHOT"
-  :description "Extends the core Clojure library with additional utility or
-  convenience functions."
+(defproject sinistral/mantle "0.3.0-SNAPSHOT"
+  :description "Extends the core Clojure library with additional utility or convenience functions."
   :url "https://github.com/sinistral/mantle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
