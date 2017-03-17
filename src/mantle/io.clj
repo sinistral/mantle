d97b28b81059ac706cf9aa56e47e0a158a10fea5
Marc Daya <marc@sinistral.net>
2017-03-17 11:15:37 -0600

    Add `string-input-stream`.
    Return a string as a `slurp`-able stream.

--- src/mantle/io.clj
+++ src/mantle/io.clj
@@ -1,3 +1,14 @@
 
 (ns mantle.io
+  (:import [java.io ByteArrayInputStream]))
 
+(defn string-input-stream
+  "Returns a `slurp`-able input stream that contains `s`.  This can be useful
+  as a double when testing functions that expect an input stream. The `charset`
+  may be either a `String` naming a valid charset, or an instance of
+  `java.nio.Charset` (please see the Javadoc for details).  If a charset is not
+  specified, the default charset for the platform is used."
+  [s & {:keys [charset]}]
+  (letfn [(get-bytes [s]
+            (if-not charset (.getBytes s) (.getBytes s charset)))]
+    (-> s get-bytes ByteArrayInputStream.)))
